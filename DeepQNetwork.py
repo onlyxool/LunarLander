c480280829630901e45121f927f2e771ce9a53b6
Kyle <onlyxool@gmail.com>
2024-08-05 01:05:22 -0400

    DQN done

--- DeepQNetwork.py
+++ DeepQNetwork.py
@@ -1,26 +1,6 @@
 import torch
 import torch.nn as nn
 
-class DeepQNetwork1(nn.Module):
-    def __init__(self, num_actions, num_observation, fc_size=64):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(num_observation, 12),
-            nn.ReLU(inplace=True),
-            nn.Linear(12, 8),
-            nn.ReLU(inplace=True),
-            nn.Linear(8, num_actions)
-        )
-
-        for layer in [self.layers]:
-            for module in layer:
-                if isinstance(module, nn.Linear):
-                    nn.init.kaiming_uniform_(module.weight, nonlinearity='relu')
-
-    def forward(self, x):
-        Q = self.layers(x)
-        return Q
-
 
 class DeepQNetwork(torch.nn.Module):
     def __init__(self, action_size=4, state_size=8, hidden_size=64):
